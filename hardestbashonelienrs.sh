017e1656885755f1d5d81f0026b24592b6c24f0e
Abdulla <abdullabloushi@gmail.com>
2024-10-15 10:37:03 +0400

    +2

--- hardestbashonelienrs.sh
+++ hardestbashonelienrs.sh
@@ -1,13 +1,17 @@
 #! usr/env/bin bash
 
-#Hardest one of 2023: 
+#Hardest one of 2023:
 detex sop.tex |  sed -E 's|https?://[^ ]+||g' | wc -c
 
 #Hardest one of 2024 so far:
 find . -type d -name temprepo -prune -o -name '*.py' | xargs wc -l | tail -n1 | awk '{print$1}'
 
 # Wed Sep 11 18:44:16 +04 2024 ~> :r !date
-history | awk '{print $0}' | awk '{print NF, $0}' | sort -nr | head -n 50 
+history | awk '{print $0}' | awk '{print NF, $0}' | sort -nr | head -n 50
 
 # Mon Sep 16 06:25:04 PM +04 2024
 find . -maxdepth 1 -type f ! -name claude* -exec mv {} gpt/;
+
+# Tue Oct 15 10:35:02 +04 2024
+ls ./ | awk 'BEGIN { printf "["; first=1 } { if (first) { printf "\"%s\"", $0; first=0 } else { printf ", \"%s\"", $0 } } END { print "]" }' | fish_clipboard_copy
+
